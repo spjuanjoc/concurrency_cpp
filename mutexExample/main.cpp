e108de50a8c21504fabf44a24104112b655f8a3e
Juan Jose Castellanos <juan.castellanos@smartmatic.com>
2020-01-23 16:16:36 -0500

    template and print

--- mutexExample/main.cpp
+++ mutexExample/main.cpp
@@ -3,23 +3,32 @@
 #include <list>
 #include <mutex>
 
+template<typename T>
 class SafeList
 {
 private:
-    std::list<int> some_list;
+    std::list<T> some_list;
     std::mutex     some_mutex;
 
 public:
-    void add_to_list( int new_value )
+    void add_to_list( T new_value )
     {
         std::lock_guard<std::mutex> guard( some_mutex );
         //    std::lock_guard guard(some_mutex); // since C++17
         some_list.push_back( new_value );
     }
-    bool list_contains( int value_to_find )
+    bool list_contains( T value_to_find )
     {
         std::lock_guard<std::mutex> guard( some_mutex );
         return std::find( some_list.begin(), some_list.end(), value_to_find ) != some_list.end();
+    }
+    void print() const
+    {
+        for (const auto& it : some_list)
+        {
+            std::cout << it << " ";
+        }
+        std::cout << "\n";
     }
 };
 
@@ -27,13 +36,16 @@ int main()
 {
     std::cout << "Hello, Concurrency!" << std::endl;
 
-    SafeList sl;
+    SafeList<int> sl;
     sl.add_to_list( 1 );
 
     int  find  = 1;
     auto found = sl.list_contains( find );
-
     std::cout << std::boolalpha << find << " exists?: " << found << std::endl;
+    int  find2  = 2;
+    auto found2 = sl.list_contains( find2 );
+    std::cout << std::boolalpha << find2 << " exists?: " << found2 << std::endl;
 
+    sl.print();
     return 0;
 }
