e803b5b2d49d0ff592fcaa6749e0d938e6297171
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-04-23 20:18:52 -0500

    add tests structure

---
+++ async_pool/test/test.foo.cpp
@@ -0,0 +1,9 @@
+#include "catch2/catch.hpp"
+#include <optional>
+#include "ParallelAccumulate.hpp"
+
+TEST_CASE("test the execution of foo", "[foo]")
+{
+  REQUIRE( 1 == 1);
+  std::optional<int> var;
+}
