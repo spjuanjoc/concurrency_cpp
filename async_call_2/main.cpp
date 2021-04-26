8fb731ccc4ed100051b60d31e0695b8b5303f1b5
Juan Jose Castellanos <juancho.312@hotmail.com>
2021-04-26 18:28:13 -0500

    refactor to modern cmake, and rename project

---
+++ async_call_2/main.cpp
@@ -0,0 +1,50 @@
+#include "fmt/core.h"
+#include <chrono>
+#include <future>
+#include <iostream>
+
+using namespace std::chrono_literals;
+
+void firstTask()
+{
+  fmt::print("First message before sleep.\n");
+  std::this_thread::sleep_for(2s);
+  fmt::print("Second message after sleep.\n");
+}
+
+void called_from_async(std::promise<void> started)
+{
+  fmt::print("called_from_async init.\n");
+  std::this_thread::sleep_for(5s);
+  fmt::print("called_from_async end.\n");
+}
+
+int main()
+{
+  std::promise<void> runnerStarted;
+  auto               runnerValue = runnerStarted.get_future();
+  std::future<void>  result;
+  result       = std::async(std::launch::async | std::launch::deferred, called_from_async, std::move(runnerStarted));
+  auto timeout = 10s;
+
+  if (result.valid())
+  {
+    if (result.wait_for(timeout) == std::future_status::ready)
+    {
+      fmt::print("ready\n");
+    }
+
+    if (result.wait_for(timeout) == std::future_status::timeout)
+    {
+      fmt::print("timeout\n");
+    }
+    else
+    {
+      fmt::print("Task done\n");
+    }
+  }
+
+  firstTask();
+
+  return 0;
+}
